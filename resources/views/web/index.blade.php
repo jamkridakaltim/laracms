52cd9f459fe4e71ce11d778ef5850a1c26795449
prihutomo <prihutomoh@gmail.com>
2021-05-31 08:14:55 +0800

    update index

--- resources/views/web/index.blade.php
+++ resources/views/web/index.blade.php
@@ -1,11 +1,13 @@
 @extends('web._layouts.app')
 @section('top-content')
 <div id="carouselExampleIndicators" class="carousel slide bg-light d-none d-sm-block" data-ride="carousel">
+
   <ol class="carousel-indicators">
     <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
     <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
     <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
   </ol>
+
   <div class="carousel-inner">
     <div class="carousel-item active">
       <div class="media p-3">
@@ -35,6 +37,7 @@
       </div>
     </div>
   </div>
+
   <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
     <span class="sr-only">Previous</span>
